2b202b95723b8cacaf7da072e4dc51f341edb5ff
Mina Youaness <minaragaie@hotmail.com>
2025-09-06 13:47:43 -0400

    see some logs

--- api/send-email.ts
+++ api/send-email.ts
@@ -51,6 +51,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   };
 
   try {
+    console.log("Sending email...");
     await transporter.sendMail(mailOptions);
     console.log("Email sent successfully!");
     res.status(200).json({ message: "Email sent successfully" });
